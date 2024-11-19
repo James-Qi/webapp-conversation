4e335cf4a8e577122f1b41a6ca1dc7e2d896c4a4
James Qi <qijingsong@gmail.com>
2024-11-19 14:52:01 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'James Qi Chat APP',
-  description: 'Hello',
+  title: '祁劲松的博客聊天机器人 🤖 Chat APP',
+  description: 'Hello，你好👋，欢迎聊天',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
